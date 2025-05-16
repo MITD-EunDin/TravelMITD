135743c0b968e6fc78bd3062fde589e5a69035d0
MITD-EunDin <tondien1104@gmail>
2025-05-16 23:01:44 +0700

    thêm biến môi trường

--- src/api/BookingApi.js
+++ src/api/BookingApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = REACT_APP_API_URL || 'http://localhost:8080';
 
 const API = axios.create({
     baseURL: API_URL,
